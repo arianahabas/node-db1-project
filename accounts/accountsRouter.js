8e9c03eb66e97e747411356a7918d967dc438334
Ariana Habas <65680650+arianahabas@users.noreply.github.com>
2020-10-15 13:50:08 -0500

    MVP complete

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -32,17 +32,73 @@ router.get("/:id", async (req, res, next) => {
 //GET account by ID
 
 router.post("/", async (req, res, next) => {
+    try {
+        const payload ={
+            name: req.body.name,
+            budget: req.body.budget,
+        }
+        if(!payload.name || !payload.budget){
+            res.status(400).json({
+                message: "Need a name and a budget"
+            })
+        }
+        const [id] = await db
+        .insert(payload)
+        .into('accounts')
 
+        const account = await db
+        .first("*")
+        .from("accounts")
+        .where("id", id)
+        
+        res.status(201).json(account)
+    } catch (err) {
+        next(err)
+    }
 })
 //CREATE new account
 
 router.put("/:id", async (req, res, next) => {
+    try {
+        const payload ={
+            name: req.body.name,
+            budget: req.body.budget,
+        }
+        if(!payload.name || !payload.budget){
+            res.status(400).json({
+                message: "Need a name and a budget"
+            })
+        }
+        //UPDATE accounts SET name=? AND budget=? WHERE id=?
+        await db('accounts')
+        .update(payload)
+        .where('id', req.params.id)
+        
+        const account = await db
+        .first("*")
+        .from("accounts")
+        .where("id", req.params.id)
 
+        res.json(account)
+
+    } catch (err) {
+        next(err)
+    }
 })
 //EDIT an account
 
 router.delete("/:id", async (req, res, next) => {
+    try {
+        //DELETE FROM accounts WHERE id = ?
+        await db('accounts')
+        .where('id', req.params.id)
+        .del()
 
+        res.status(204).end()
+
+    } catch (err) {
+        next(err)
+    }
 })
 //DELETE an account
 
